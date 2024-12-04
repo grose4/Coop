45b5a9b0f6a6b4a5d3712cf47f569c991f8676c7
grose4 <gjr417@gmail.com>
2024-12-03 22:49:07 -0500

    Update user2_routes.py

--- api/backend/user2_routes.py
+++ api/backend/user2_routes.py
@@ -10,22 +10,72 @@ from backend.ml_models.model01 import predict
 users = Blueprint('customers', __name__)
 
 # Get users from specific industries:
-@users.route('/users/<userID>', methods=['PUT'])
-def get_users_by_industry():
+@users.route('/users/<industry>', methods=['GET'])
+def get_users_by_industry(industry):
+    
+    query = f'''
+    SELECT u.Name, u.Bio, i.Name AS Industry, i.NUCollege
+    FROM Users u
+	JOIN User_Industry ui ON u.UserID = ui.UserID
+	JOIN Industry i ON i.IndustryID = ui.IndustryID
+    WHERE i.Industry = {industry}
+    '''
 
+    current_app.logger.info('GET /users/<Industry> route')
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+# update user info
+@users.route('/users/<userID>', methods=['PUT'])
+def update_user(userID):
     # log and make cursor
     current_app.logger.info('PUT /users/<userID> route')
     cursor = db.get_db().cursor()
 
-    user_id = user_info['userID']
-    occupation = user_info['occupation']
+    # set variables for each user attribute
+    user_info = request.json
     name = user_info['name']
-    company = user_info['company']
+    occupation = user_info['occupation']
+    location = user_info['location']
+    age = user_info['age']
+    bio = user_info['bio']
 
-    query = 'UPDATE customers SET first_name = %s, last_name = %s, company = %s where id = %s'
-    data = (first, last, company, cust_id)
-    cursor = db.get_db().cursor()
+    # execute query
+    query = 'UPDATE users SET name = %s, occupation = %s, location = %s, age = %s, bio = %s WHERE id = %s'
+    data = (name, occupation, location, age, bio, userID)
     r = cursor.execute(query, data)
     db.get_db().commit()
     return 'customer updated!'
+
+# search users by their skills
+@users.route('/users/by-skills', methods=['GET'])
+def get_users_by_skills():
+    
+    soft_skills = request.args.get('soft_skills')
+    tech_skills = request.args.get('tech_skills')
+
+    query = '''
+    SELECT u.Name, u.Bio, u.Occupation, c.CompanyName, e.SoftSkills, e.TechnicalSkills
+    FROM Users u
+	JOIN User_Type ut ON u.UserID = ut.UserID
+	JOIN Employers e ON ut.EmpID = e.EmpID
+	JOIN Companies c ON c.CompanyID = e.EmpID
+    WHERE e.SoftSkills = %s AND e.TechnicalSkills = %s
+    '''
+
+    current_app.logger.info('GET /users/by-skills route')
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
 
