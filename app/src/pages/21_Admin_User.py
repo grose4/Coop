70aafd5a230bfbcf61dc53e3d76d9c309d4da3e3
jbenes3 <150630164+jbenes3@users.noreply.github.com>
2024-12-04 18:58:59 -0500

    Debugging on Admin User

--- app/src/pages/21_Admin_User.py
+++ app/src/pages/21_Admin_User.py
@@ -1,28 +1,27 @@
+import logging
 import streamlit as st
 import pandas as pd
 import requests
 
-API_URL = "http://web-api:4000/p"
+logger = logging.getLogger(__name__)
 
-def fetch_users():
-    """
-    Fetch user data from the REST API.
-    Returns a Pandas DataFrame with the user data.
-    """
-        response = requests.get(f"{API_URL}/users")
-        response.raise_for_status()
-        users = response.json()
-        return pd.DataFrame(users, columns=['id', 'username', 'email'])
+API_URL = "http://api:4000/users"
 
 def main():
     st.title("User Management Dashboard")
-    
+
     st.header("User Data Table")
     st.write("Below is the list of all users fetched from the REST API.")
-    
-    user_data = fetch_users()
+
+    user_data = pd.DataFrame()
+    response = requests.get({API_URL})
+    response.raise_for_status()  
+    users = response.json()
+    user_data = pd.DataFrame(users, columns=["id", "username", "email"])
+    st.write("Successfully fetched data from the API.")
+   
     if not user_data.empty:
-        st.dataframe(user_data) 
+        st.dataframe(user_data)
     else:
         st.write("No user data available.")
 
