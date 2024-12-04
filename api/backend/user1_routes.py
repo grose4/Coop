12f8a436a01954ebbee154de3d73a8a4397ce0c6
jbenes3 <150630164+jbenes3@users.noreply.github.com>
2024-12-03 22:55:33 -0500

    Cleaned up file, removed unneeded imports

--- api/backend/user1_routes.py
+++ api/backend/user1_routes.py
@@ -1,14 +1,9 @@
-########################################################
-# Sample customers blueprint of endpoints
-# Remove this file if you are not using it in your project
-########################################################
+
 from flask import Blueprint
 from flask import request
 from flask import jsonify
 from flask import make_response
-from flask import current_app
 from backend.db_connection import db
-from backend.ml_models.model01 import predict
 
 # Create a new Blueprint object
 api = Blueprint('api', __name__)
