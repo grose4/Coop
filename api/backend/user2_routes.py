f5f8e4ddda160f7c21e63d17677b55f2e5f88900
grose4 <gjr417@gmail.com>
2024-12-03 22:07:00 -0500

    updating routes

--- api/backend/user2_routes.py
+++ api/backend/user2_routes.py
@@ -11,16 +11,16 @@ users = Blueprint('customers', __name__)
 
 # Get users from specific industries:
 @users.route('/users/<userID>', methods=['PUT'])
-def get_users_by_industry(userID):
+def get_users_by_industry():
 
     # log and make cursor
     current_app.logger.info('PUT /users/<userID> route')
     cursor = db.get_db().cursor()
 
-    user_id = cust_info['userID']
-    first = cust_info['first_name']
-    last = cust_info['last_name']
-    company = cust_info['company']
+    user_id = user_info['userID']
+    occupation = user_info['occupation']
+    name = user_info['name']
+    company = user_info['company']
 
     query = 'UPDATE customers SET first_name = %s, last_name = %s, company = %s where id = %s'
     data = (first, last, company, cust_id)
