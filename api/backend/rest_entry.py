3ff0a2449a0b71bd3b12a93a90d7376b90780098
jbenes3 <150630164+jbenes3@users.noreply.github.com>
2024-12-04 23:38:24 -0500

    Further debugging, trying to allow sql to connect to api

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from backend.db_connection import db
-from backend.customers.customer_routes import customers
+from backend.user1_routes import api
 from backend.products.products_routes import products
 from backend.simple.simple_routes import simple_routes
 import os
@@ -20,16 +20,15 @@ def create_app():
     # secret key that will be used for securely signing the session 
     # cookie and can be used for any other security related needs by 
     # extensions or your application
-    # app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
+    app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
     # # these are for the DB object to be able to connect to MySQL. 
-    # app.config['MYSQL_DATABASE_USER'] = 'root'
-    app.config['MYSQL_DATABASE_USER'] = os.getenv('DB_USER').strip()
+    app.config['MYSQL_DATABASE_USER'] = 'root'
     app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_ROOT_PASSWORD').strip()
     app.config['MYSQL_DATABASE_HOST'] = os.getenv('DB_HOST').strip()
     app.config['MYSQL_DATABASE_PORT'] = int(os.getenv('DB_PORT').strip())
-    app.config['MYSQL_DATABASE_DB'] = os.getenv('DB_NAME').strip()  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'finalpart2' # Change this to your DB name
 
     # Initialize the database object with the settings above. 
     app.logger.info('current_app(): starting the database connection')
@@ -40,9 +39,8 @@ def create_app():
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
     app.register_blueprint(simple_routes)
-    app.register_blueprint(customers,   url_prefix='/c')
+    app.register_blueprint(api, url_prefix='/a')
     app.register_blueprint(products,    url_prefix='/p')
 
     # Don't forget to return the app object
     return app
-
