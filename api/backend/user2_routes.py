2d20b651f4d91a0260f305090fb6e851126b4ac6
jbenes3 <150630164+jbenes3@users.noreply.github.com>
2024-12-06 15:51:03 -0500

    Fixed merge error

--- api/backend/user2_routes.py
+++ api/backend/user2_routes.py
@@ -1,8 +1,8 @@
-from flask import Flask 
 from flask import Blueprint
 from flask import request
 from flask import jsonify
 from flask import make_response
+from flask import current_app
 from backend.db_connection import db
 import logging
 
