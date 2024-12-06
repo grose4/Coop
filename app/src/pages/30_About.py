19cb478a38c6de03bb39f4e3fe2475707985d272
jbenes3 <150630164+jbenes3@users.noreply.github.com>
2024-12-06 15:34:31 -0500

    Changed about

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -10,11 +10,7 @@ st.write("# About this App")
 
 st.markdown (
     """
-<<<<<<< HEAD
     This is a Co-Op Search & Networking App for our CS 3200 Course Project.  
-=======
-    This is a demo app for a CS 3200 Course Project.  
->>>>>>> c1364949e97ee85a7b5c69279f8340a1a35a441a
 
     The goal of this app is to provide an easier way for students and
     faculty alike to network and find the perfect co-op.
