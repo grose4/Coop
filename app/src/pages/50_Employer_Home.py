29290f1893114176ecdf3a5b515fc103379ba6f4
eeshneun <148018955+eeshneun@users.noreply.github.com>
2024-12-07 09:24:02 -0500

    fixing logo

--- app/src/pages/50_Employer_Home.py
+++ app/src/pages/50_Employer_Home.py
@@ -3,9 +3,11 @@ logger = logging.getLogger(__name__)
 
 import streamlit as st
 from modules.nav import SideBarLinks
+import requests
 
-st.set_page_config(layout='wide')
-SideBarLinks()
+st.set_page_config(layout = 'wide')
+
+SideBarLinks() 
 
 st.title('Employer Home')
 
